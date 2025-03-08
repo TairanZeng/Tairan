f102aaf05041d3dd233ce929c54f36c4f9b6228b
TairanZeng <tairan.zeng@gmail.com>
2025-03-08 10:29:05 +0400

    Create ex02.cpp
    ex02

---
+++ ex02.cpp
@@ -0,0 +1,25 @@
+
+========= practice 2   ==================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int DaddyCash = 10;
+int TairanCash = 10;
+
+if (DaddyCash > TairanCash) {
+cout << "Daddy is richer." << endl;
+}
+else if (DaddyCash < TairanCash)
+{
+cout << "Tairan is richer." << endl;
+}
+else
+{
+cout << "They are equally rich." << endl;
+} return 0;
+
+}
+
